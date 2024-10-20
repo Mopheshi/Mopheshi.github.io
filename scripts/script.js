c85872a567b8faaab97ffe7c3b1f82bc37a07032
Mophe <87334468+Mopheshi@users.noreply.github.com>
2024-10-20 14:11:19 +0100

    Updated UI and structure...

--- scripts/script.js
+++ scripts/script.js
@@ -9,39 +9,24 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    // Counting effect
     const counters = document.querySelectorAll('.stats h3');
     const speed = 200;
 
-    const countUp = (counter) => {
-        const updateCount = () => {
+    counters.forEach(counter => {
+        const countUp = () => {
             const target = parseInt(counter.getAttribute('data-target'), 10);
-            const count = parseInt(counter.innerText.replace(/\D/g, ''), 10);
+            const count = parseInt(counter.innerText, 10);
+
             const increment = target / speed;
 
             if (count < target) {
-                counter.innerText = `${Math.ceil(count + increment)}+`;
-                setTimeout(updateCount, 1);
+                counter.innerText = Math.ceil(count + increment);
+                setTimeout(countUp, 30);
             } else {
-                counter.innerText = `${target}+`;
+                counter.innerText = target;
             }
         };
 
-        updateCount();
-    };
-
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                countUp(entry.target);
-                observer.unobserve(entry.target);
-            }
-        });
-    }, {threshold: 0.5});
-
-    counters.forEach(counter => {
-        counter.setAttribute('data-target', counter.innerText.replace(/\D/g, ''));
-        counter.innerText = '0';
-        observer.observe(counter);
+        countUp();
     });
 });
